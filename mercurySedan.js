1afccc0d793565225f828c4aee0d7abf24f7a13e
Conor Panuccio <conorpanu@hotmail.com>
2023-05-31 14:38:08 -0400

    finished

--- mercurySedan.js
+++ mercurySedan.js
@@ -2,8 +2,7 @@
 const VehicleModule = require("./vehicleBaseClass")
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+
 
 
 //After you write the derived Car class, you should test it out.
@@ -13,16 +12,39 @@ console.log(v.make)
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+class Car extends VehicleModule.Vehicle {
+    constructor(make, model, year, color, mileage, maximumPassengers, passengers, numberOfWheels, maximumSpeed, fuel){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = maximumPassengers
+        this.passengers = passengers
+        this.numberOfWheels = numberOfWheels
+        this.maximumSpeed = maximumSpeed
+        this.fuel = fuel
+        this.scheduleService = false
+    }
+    loadPassenger(passengers){
+        if(this.passengers < 5 && (this.passengers + passengers) < 5){
+            this.passengers = passengers + this.passengers
+            console.log(this.passengers + " passengers in the car")
+        }else{
+            console.log("to many people for the car")
+        }
+    }
 
+    checkService(){
+        if(this.mileage > 30000){
+            console.log("Time for service")
+            return this.scheduleService = true
+        }else{
+            console.log("car is fine")
+        }
+    }
+}
+let v = new Car("Mercury", "Sedan", "1965", "color", 15000, 5, 0, 4, 160, 10);
 
-
-
-
-
-
-
-
-
+console.log(v.fuel)
+v.start()
+v.checkService(32000)
 
 //TO DO: Creating Instances and Testing Them
 
